50373e0fae7a9ba7f4f5948cff8ca9c700d842aa
Harshsaini6681 <harshsaini6681@gmail.com>
2026-06-28 21:59:49 +0530

    DAY-28 ASSIGNMENT UPLOADED.

---
+++ DAY-28/Q111.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+struct Ticket 
+{
+    int ticketNo;
+    char name[20];
+    int seats;
+};
+int main() 
+{
+    struct Ticket t;
+    printf("Enter Ticket Number: ");
+    scanf("%d", &t.ticketNo);
+    printf("Enter Passenger Name: ");
+    scanf("%s", t.name);
+    printf("Enter Number of Seats: ");
+    scanf("%d", &t.seats);
+    printf("\n--- Ticket Details ---\n");
+    printf("Ticket Number : %d\n", t.ticketNo);
+    printf("Passenger Name: %s\n", t.name);
+    printf("Seats Booked  : %d\n", t.seats);
+    return 0;
+}
